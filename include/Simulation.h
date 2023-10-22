40ef2da298bf8e902ac931e02b95fc7ab59d2ab8
Meoli96 <robs96rm@gmail.com>
2023-10-22 17:15:54 +0200

    Added drop dms

--- include/Simulation.h
+++ include/Simulation.h
@@ -6,41 +6,37 @@
 
 #include "Model.h"
 #include "State.h"
-
+#include "Waypoint.h"
 class Simulation {
-private:
-  // Methods parameters
-  std::string method;
-  double time_step;
-  double time_interval;
-  Model h;
+   private:
+    // Methods parameters
+    std::string method;
+    double time_step;
+    double time_interval;
+    Model h;
 
-  // Simulation results
-  std::vector<State> res;
+    // Simulation results
+    std::vector<State> res;
 
-public:
-  Simulation(double dt, double T, std::string method, Model _h, State S0);
-  Simulation(double dt, double T, std::string method, Model _h, double z,
-             double Vmod, double heading);
-  /*
-  Simulation(std::string setting_fle, double Vmod, double heading)
-  
+   public:
+    Simulation(double dt, double T, std::string method, Model _h, State S0);
+    Simulation(double dt, double T, std::string method, Model _h, double z,
+               double Vmod, double heading);
+    /*
+    Simulation(std::string setting_fle, double Vmod, double heading)
 
-  
-  */  
 
-  ~Simulation() {}
 
-  // ODE
+    */
 
-  std::vector<State> retRes() { return res; }
-  void write_state(const State s, double t);
-  void run();
+    ~Simulation() {}
 
-  /*
-  // void get_waypoints() {
-  //    run();
-  //    Compute 3 waypoint needed by autopilot
-  // }
-  */
+    // ODE
+
+    std::vector<State> retRes() { return res; }
+    void write_state(const State s, double t);
+    void run();
+
+    std::vector<DMS> get_waypoints(const DMS &dms, double heading,
+                                   double distance);
 };
--- include/Waypoint.h
+++ include/Waypoint.h
@@ -1,4 +1,38 @@
-typedef struct  {
-  double lat;
-  double lon;
-} Waypoint;
+#include <cmath>
+
+typedef struct {
+    double lat = 0;
+    double lon = 0;
+
+
+} GPS;
+
+typedef struct {
+    double N[3] = {0};
+    double E[3] = {0};
+} DMS;
+
+GPS dms2gps(const DMS &dms) {
+    GPS ret;
+    for (int i = 0; i < 3; i++) {
+        ret.lat += dms.N[i] / pow(60, i + 1);
+        ret.lon += dms.E[i] / pow(60, i + 1);
+    }
+    return ret;
+}
+
+DMS gps2dms(const GPS &gps) {
+    DMS ret;
+    
+    ret.N[0] = floor(gps.lat);
+    ret.E[0] = floor(gps.lon);
+    
+    GPS temp{(gps.lat - floor(gps.lat)) * 60, (gps.lon - floor(gps.lon)) * 60};
+    ret.N[1] = floor(temp.lat);
+    ret.E[1] = floor(temp.lon);
+
+    ret.N[2] = (temp.lat - floor(temp.lat))*60;
+    ret.E[2] = (temp.lon - floor(temp.lon))*60;
+
+    return ret;
+}
