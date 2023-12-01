e80356da4c9e1c189b248236697b2b0640dabbb5
StefanoMagriniAlunno <stefanomagrini99@gmail.com>
2023-12-01 21:34:19 +0100

    Primo algoritmo per la convalida

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -7,18 +7,18 @@
 /**
  * @brief initialize and run the model
  *
- * @param size : num of launches
- * @param wind : {wind[2*i+0], wind[2*i+1]} is {degree, m/s} of the wind (remind: 0° = the wind go to Sud from North)
- * @param vel : {vel[3*i+0], vel[3*i+1], vel[3*i+2]} is {heading, magnitude, v_down} of the UAV velocity
- * @param target : {target[2*i+0], target[2*i+1]} is the GPS position of the target
- * @param h : h[i] is the altitude (m)
- * @param m : m[i] is the mass of the payload (Kg)
- * @param CdS : is the coefficient of the parachute (m2)
- * @param time : time of the simulation (s)
- * @param step : step of the simulation (s)
+ * @param size       : num of launches
+ * @param wind       : {wind[2*i+0], wind[2*i+1]} is {degree, m/s} of the wind (remind: 0° = the wind go to Sud from North)
+ * @param vel        : {vel[3*i+0], vel[3*i+1], vel[3*i+2]} is {heading, magnitude, v_down} of the UAV velocity
+ * @param target     : {target[2*i+0], target[2*i+1]} is the GPS position of the target
+ * @param h          : h[i] is the altitude (m)
+ * @param m          : m[i] is the mass of the payload (Kg)
+ * @param CdS        : is the coefficient of the parachute (m2)
+ * @param time       : time of the simulation (s)
+ * @param step       : step of the simulation (s)
  * @param integrator : integrator of the simulation
- * @param result : {result[2*i+0], result[2*i+1]} GPS coordinate of the launch
- * @return int : 0 = anyError, 1 = anErrorEncountered
+ * @param result     : {result[2*i+0], result[2*i+1]} GPS coordinate of the launch
+ * @return int       : 0 = anyError, 1 = anErrorEncountered
  */
 int
 cxx_run(int size, double* wind, double* vel, double* target, double* h, double* m, double CdS, double time, double step, const char* integrator, double* result)
@@ -35,7 +35,7 @@ extern "C" {
 
 #include <stdio.h>
 
-#define KT2M 0.541 /* conversione da nodi a metri al secondo: 1 nodo = 0.541 m/s */
+#define KT2M 0.541 /* 1 knot = 0.541 m/s */
 
 static PyObject*
 run(PyObject *self, PyObject *args)
@@ -47,7 +47,7 @@ run(PyObject *self, PyObject *args)
         *target,     /* GPS target position: [latitude, longitude]        */
         *h,          /* altitude of UAV (in m)                            */
         *m;          /* Mass of payloads (in g)                           */
-    float CdS;       /* CdS coefficient of parachutes  */
+    double CdS;      /* CdS coefficient of parachutes  */
     int time,        /* Time of the simulation (in s)  */
         step;        /* Step of the simulation (in ms) */
     const char* integrator; /* integrator */
@@ -59,11 +59,11 @@ run(PyObject *self, PyObject *args)
     /* check the objects */
     {
         int
-            flag_wind  = !PyList_Check(wind),
-            flag_vel   = !PyList_Check(vel),
-            flag_target   = !PyList_Check(target),
-            flag_h     = !PyList_Check(h),
-            flag_m     = !PyList_Check(m);
+            flag_wind   = !PyList_Check(wind),
+            flag_vel    = !PyList_Check(vel),
+            flag_target = !PyList_Check(target),
+            flag_h      = !PyList_Check(h),
+            flag_m      = !PyList_Check(m);
         if (flag_wind || flag_vel || flag_target || flag_h || flag_m) {
             return NULL;
         }
@@ -71,11 +71,11 @@ run(PyObject *self, PyObject *args)
 
     /* check size */
     Py_ssize_t size = 0;
-    if (size == 0) size = PyList_GET_SIZE(wind); else if (size != PyList_GET_SIZE(wind)) return NULL;
-    if (size == 0) size = PyList_GET_SIZE(vel);  else if (size != PyList_GET_SIZE(vel))  return NULL;
+    if (size == 0) size = PyList_GET_SIZE(wind);    else if (size != PyList_GET_SIZE(wind))    return NULL;
+    if (size == 0) size = PyList_GET_SIZE(vel);     else if (size != PyList_GET_SIZE(vel))     return NULL;
     if (size == 0) size = PyList_GET_SIZE(target);  else if (size != PyList_GET_SIZE(target))  return NULL;
-    if (size == 0) size = PyList_GET_SIZE(h);    else if (size != PyList_GET_SIZE(h))    return NULL;
-    if (size == 0) size = PyList_GET_SIZE(m);    else if (size != PyList_GET_SIZE(m))    return NULL;
+    if (size == 0) size = PyList_GET_SIZE(h);       else if (size != PyList_GET_SIZE(h))       return NULL;
+    if (size == 0) size = PyList_GET_SIZE(m);       else if (size != PyList_GET_SIZE(m))       return NULL;
     if (size == 0) return NULL;
 
     /* alloc memory */
@@ -102,12 +102,12 @@ run(PyObject *self, PyObject *args)
             c_wind[2*i+1] = speed_knots * KT2M;
         }
         if (!PyTuple_Check(item_vel) || PyTuple_GET_SIZE(item_vel) != 3) return NULL;
-        c_vel[3*i] = PyFloat_AsDouble(PyTuple_GET_ITEM(item_vel, 0));
+        c_vel[3*i+0] = PyFloat_AsDouble(PyTuple_GET_ITEM(item_vel, 0));
         c_vel[3*i+1] = PyFloat_AsDouble(PyTuple_GET_ITEM(item_vel, 1));
         c_vel[3*i+2] = PyFloat_AsDouble(PyTuple_GET_ITEM(item_vel, 2));
 
         if (!PyTuple_Check(item_target) || PyTuple_GET_SIZE(item_target) != 2) return NULL;
-        c_target[2*i] = PyFloat_AsDouble(PyTuple_GET_ITEM(item_target, 0));
+        c_target[2*i+0] = PyFloat_AsDouble(PyTuple_GET_ITEM(item_target, 0));
         c_target[2*i+1] = PyFloat_AsDouble(PyTuple_GET_ITEM(item_target, 1));
 
         c_h[i] = PyFloat_AsDouble(PyList_GET_ITEM(h, i));
@@ -123,8 +123,11 @@ run(PyObject *self, PyObject *args)
     /* convert result in List */
     PyObject* resultObj = PyList_New(size);
     for (Py_ssize_t i = 0; i < size; ++i) {
-        PyObject* item_result = Py_BuildValue("(ff)", PyFloat_FromDouble(result[2*i]), PyFloat_FromDouble(result[2*i+1]));
-        PyList_SetItem(resultObj, i, item_result);
+        PyObject* item_result = PyTuple_New(2);
+        if (!item_result) return NULL;
+        PyTuple_SET_ITEM(item_result, 0, PyFloat_FromDouble(result[2*i+0]));
+        PyTuple_SET_ITEM(item_result, 1, PyFloat_FromDouble(result[2*i+1]));
+        PyList_SET_ITEM(resultObj, i, item_result);
     }
 
     free(result);
--- test.py
+++ test.py
@@ -2,19 +2,29 @@
 
 import libsft_fall_model as ad
 import pandas as pd
-"""
+import geopy.distance as gp_dist
+
 # read file with all data
 file_name = "test2.csv"
 empirical_data = pd.read_csv(file_name, sep='\s+')
 
 # algorithm
 def compute_loss(CdS, data):
+    # convert data in list
     altitude = empirical_data['H'].tolist()
     mass = empirical_data['mass'].tolist()
     target = list(zip(empirical_data['Lat_land'], empirical_data['Long_land']))
     vel = list(zip(empirical_data['Head'], empirical_data['Vel'], empirical_data['V_down']))
     wind = empirical_data['wind'].tolist()
     res = ad.run(wind, vel, target, altitude, mass, CdS, 10, 2, 'rk4')
+    # compute loss
+    sum = 0
+    for launch in range(len(res)):
+        theoretical_launch_point = res[launch]
+        empirical_launch_point = (empirical_data['Lat_drop'][launch], empirical_data['Long_drop'][launch])
+        distance = gp_dist.geodesic(theoretical_launch_point, empirical_launch_point).meters
+        sum += distance**2
+    loss = (sum/len(res))**0.5
     return loss
 
 initial_CdS = 2
@@ -37,18 +47,8 @@ while step > final_step:
         CdS = CdS+directions[best_loss_index]*step
     else:
         step = step/2
-"""
-wind_str = "18003KT"            # il vento si descrive dicendo da dove viene e si indica con una stringa "{gradi}{nodi}KT" (es. "24010KT": il vento viene da 240° a velocità 10 nodi)
-vel_list = (2.0, 22.0, -2)      # velocità vettoriale descritta come direzione (in radianti N/NE) e velocità in m/s
-pos = (41.8317465, 12.3334762)  # posizione del target espressa come latitudine e longitudine
-h = 33.9                        # quota di lancio in metri
-m = 0.560                       # massa in chilogrammi del paracadute+payload
-Cd = 1.5                        # cd del paracadute
-S = 0.28                        # superficie del paracadute in m2
-time = 10                       # il tempo in secondi
-step = 2                        # lo step in millisecondi
-integrator = "rk4"              # integrator (es. "rk4", "rk45", "euler")
 
-res = ad.run([wind_str], [vel_list], [pos], [h], [m], Cd*S, time, step, integrator)
-
-print(res)
+print("CdS trovato: ")
+print(CdS)
+print("Errore quadratico medio: ")
+print(curr_loss)
